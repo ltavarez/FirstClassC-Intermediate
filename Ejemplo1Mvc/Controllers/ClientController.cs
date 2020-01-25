c117c87d4e3ade1e27c9a644a26cf74ad824815d
Leonardo <leonardo.tavarez@necomplus.com>
2020-01-25 11:48:21 -0400

    Initial commit

---
+++ Ejemplo1Mvc/Controllers/ClientController.cs
@@ -0,0 +1,29 @@
+﻿using Ejemplo1Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace Ejemplo1Mvc.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            List<Person> persons = new List<Person>{};
+
+
+            ViewBag.HaveData = persons.Any();
+            ViewBag.PersonList = persons;
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ Ejemplo1Mvc/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ejemplo1Mvc.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
